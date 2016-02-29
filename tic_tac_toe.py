dce545e6963fac8e0606a32b82369b63d30f35a1
Chuhan Feng <simonrouse9461@gmail.com>
2016-02-29 16:10:16 -0500

    update eval

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -115,9 +115,18 @@ class TicTacToeProblem(ProblemFormulation):
 
     @classmethod
     def eval(cls, state, player):
-        x1, x2, o1, o2 = [0] * 4
-        for first, second, third in state:
+        p1, p2, a1, a2 = [0] * 4
+        for line in state:
+            p_count = line.count(player)
+            a_count = line.count(3 - player)
+            if p_count == 2 and a_count == 0:
+                p2 += 1
+            if p_count == 1 and a_count == 0:
+                p1 += 1
+            if p_count == 0 and a_count == 2:
+                a2 += 1
+            if p_count == 0 and a_count == 1:
+                a1 += 1
+        return 3 * p2 + p1 - (3 * a2 + a1)
 
-            p_count = 0
-            a_count = 0
 
